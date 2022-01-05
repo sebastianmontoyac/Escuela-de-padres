f8485384064b3f6d162b0ca50a16d3bbcc161c14
Juan Sebastian Montoya Casanova <sebastianmontoya60@hotmail.com>
2022-01-05 00:04:49 -0500

    pagina web de la escuela de padres

---
+++ Primaria.js
@@ -0,0 +1,243 @@
+function myFunction() {
+  let click = document.getElementById("presentaciones");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function tituloCanciones() {
+  let click = document.getElementById("canciones");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function tituloVideos() {
+  let click = document.getElementById("videos");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function videosConcienciaCorporal() {
+  let click = document.getElementById("videosh4");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function myFunction4() {
+  let click = document.getElementById("cuentos");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function myFunction5() {
+  let click = document.getElementById("actividades");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+
+function myFunction6() {
+  let click = document.getElementById("presentacion1");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function myFunction7() {
+  let click = document.getElementById("presentacion2");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function myFunction8() {
+  let click = document.getElementById("presentacion3");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function myFunction9() {
+  let click = document.getElementById("presentacion4");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function myFunctionCuento1() {
+  let click = document.getElementById("cuento1");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function myFunctionCuento2() {
+  let click = document.getElementById("cuento2");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function myFunctionCuento3() {
+  let click = document.getElementById("cuento3");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function myFunctionCUento4() {
+  let click = document.getElementById("cuento4");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+
+function cancion1() {
+  let click = document.getElementById("cancion1");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function cancion2() {
+  let click = document.getElementById("cancion2");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function cancion3() {
+  let click = document.getElementById("cancion3");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function cancion4() {
+  let click = document.getElementById("cancion4");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function cancion5() {
+  let click = document.getElementById("cancion5");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function cancion6() {
+  let click = document.getElementById("cancion6");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function cancion7() {
+  let click = document.getElementById("cancion7");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function cancion8() {
+  let click = document.getElementById("cancion8");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function video1() {
+  let click = document.getElementById("video1");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+
+function video2() {
+  let click = document.getElementById("video2");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function video3() {
+  let click = document.getElementById("video3");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function video4() {
+  let click = document.getElementById("video4");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function video5() {
+  let click = document.getElementById("video5");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function video6() {
+  let click = document.getElementById("video6");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function video7() {
+  let click = document.getElementById("video7");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
+function video8() {
+  let click = document.getElementById("video8");
+  if (click.style.display === "none") {
+    click.style.display = "block";
+  } else {
+    click.style.display = "none";
+  }
+}
